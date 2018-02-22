5d503e712dd39e4834a547934542130f6007c8af
Aaron Evans <aaron.ptb@gmail.com>
2018-02-22 01:22:50 -0600

    plays a random directors gif every 8 seconds

--- pipeline/idea/index.js
+++ pipeline/idea/index.js
@@ -1,6 +1,35 @@
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 $("#hackathons .main a").tilt({
 	perspective: 200,
 	maxTilt: 3,
 	glare: true,
 	maxGlare: .5
 });
+
+let leader_videos = $(".leader .headshot video")
+leader_videos.on("mouseenter", (e)=>{
+	e.target.play();
+})
+
+leader_videos.on("mouseleave", (e)=>{
+	e.target.pause();
+	e.target.currentTime=0;
+})
+
+// leader_videos.on("ended", (e)=>{
+// 	e.target.currentTime=0;
+// })
+
+async function playleaders() {
+	while (true) {
+		let rand_leader = Math.floor(Math.random()*leader_videos.length)
+		leader_videos.eq(rand_leader)[0].play();
+		await sleep(8000)
+	}
+}
+
+playleaders();
