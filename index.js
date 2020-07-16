b3ab156accae7b8eeedcf160e792a8b1698962d5
Arjun Kunjilwar <arjunkunjilwar@utexas.edu>
2020-07-16 12:36:11 -0500

    Minor fixes to padding added learn more button

--- index.js
+++ index.js
@@ -9,6 +9,30 @@ window.onload = function() {
     document.querySelector('.toggle').addEventListener('click', toggleNavbar)
 }
 
+function imageHover(element) {
+    var currSrc = element.src; 
+    const path = currSrc.indexOf('img')
+    const dotPos = currSrc.indexOf('.', path)
+    var newSrc = currSrc.substring(path, dotPos) + "HOVER.png" 
+    console.log(newSrc)
+    element.setAttribute('src', newSrc)
+}
+
+function imageHover(element) {
+    var currSrc = element.src; 
+    const path = currSrc.indexOf('img')
+    const dotPos = currSrc.indexOf('.', path)
+    var newSrc = currSrc.substring(path, dotPos) + "HOVER.png" 
+    element.setAttribute('src', newSrc)
+}
+
+function imageHoverOut(element) {
+    var currSrc = element.src; 
+    const cutOut = currSrc.indexOf('HOVER')
+    var newSrc = currSrc.substring(0, cutOut) + currSrc.substring(cutOut + 5);
+    element.setAttribute('src', newSrc)
+}
+
 window.onscroll = function() {
     const navbar = document.querySelector('.navbar')
     const alpha = (window.scrollY > 100 ? 1 : window.scrollY / 100);
